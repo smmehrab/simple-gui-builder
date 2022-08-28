a6436f6779cf986fed005714d098a1521b018446
smmehrab <mehrab.24csedu.001@gmail.com>
2022-08-29 03:35:32 +0600

    repo name changed

--- src/App.java
+++ src/App.java
@@ -1,4 +1,18 @@
-public class App {
+/*
+-----------------------------------------------------
+# fullname      :   s.m.mehrabul islam
+# roll          :   sh-86
+# registration  :   2017614964
+# session       :   2017-2018
+# email         :   smmehrabul-2017614964@cs.du.ac.bd
+# department    :   computer science & engineering
+# institute     :   university of dhaka, bangladesh
+-----------------------------------------------------
+*/
+
+import javax.swing.*;
+
+public class App extends JFrame {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
