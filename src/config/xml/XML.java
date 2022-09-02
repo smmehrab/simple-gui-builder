c916475b9fbd1582cbd63a7f06bd984c9dbc355b
smmehrab <smmehrabul-2017614964@cs.du.ac.bd>
2022-09-03 04:59:44 +0600

    XML.java implemented

--- src/config/xml/XML.java
+++ src/config/xml/XML.java
@@ -1,5 +1,84 @@
+/*
+-----------------------------------------------------
+# fullname      :   s.m.mehrabul islam
+# roll          :   sh-86
+# registration  :   2017614964
+# session       :   2017-2018
+# email         :   smmehrabul-2017614964@cs.du.ac.bd
+# department    :   computer science & engineering
+# institute     :   university of dhaka, bangladesh
+-----------------------------------------------------
+
+Responsible for XML parsing and 
+XML --> Component conversion.
+
+*/
 package config.xml;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import config.component.Component;
+import config.component.ComponentType;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
 public class XML {
-    
+
+    public NodeList parse(File file) {
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = null;
+        try {
+            documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        } 
+        catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        Document document = null;
+        try {
+            document = documentBuilder.parse(file);
+        } 
+        catch (SAXException e) {
+            e.printStackTrace();
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        document.getDocumentElement().normalize();
+        NodeList nodeList = document.getElementsByTagName("component");
+        return nodeList;
+    }
+
+    public Component getComponent(Node node) {
+        ComponentType type = ComponentType.None;
+        String text = "";
+        String color = "";
+        int x = -1;
+        int y = -1;
+        
+        if (node.getNodeType() == Node.ELEMENT_NODE) {
+            Element eElement = (Element) node;
+            // Debug
+            // System.out.println("type: "+ eElement.getElementsByTagName("type").item(0).getTextContent());
+            // System.out.println("text: "+ eElement.getElementsByTagName("text").item(0).getTextContent());
+            // System.out.println("   x: "+ eElement.getElementsByTagName("x").item(0).getTextContent());
+            // System.out.println("   y: "+ eElement.getElementsByTagName("y").item(0).getTextContent());
+            type = ComponentType.fromString(eElement.getElementsByTagName("type").item(0).getTextContent());
+            text = eElement.getElementsByTagName("text").item(0).getTextContent();
+            x = Integer.parseInt(eElement.getElementsByTagName("x").item(0).getTextContent());
+            y = Integer.parseInt(eElement.getElementsByTagName("y").item(0).getTextContent());
+        }
+
+        Component component = new Component(type, text, color, x, y);
+        return component;
+    }
 }
