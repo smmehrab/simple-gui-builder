32f1ab02f94bab71b7b07dcb0488a768a4dfe798
smmehrab <smmehrabul-2017614964@cs.du.ac.bd>
2022-09-03 00:24:58 +0600

    ComponentType modified

--- src/config/component/ComponentType.java
+++ src/config/component/ComponentType.java
@@ -4,20 +4,5 @@ public enum ComponentType {
     None,
     Button,
     Editbox,
-    Textbox;
-
-    public static ComponentType fromString(String type) {
-        type.toLowerCase();
-        switch(type) {
-            case "button":
-                return Button;
-            case "editbox":
-                return Editbox;
-            case "textbox":
-                return Textbox;
-            default:
-                break;
-        }
-        return None;
-    }
+    Textbox
 }
