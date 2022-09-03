47dae977ef798684cd2e3c2fcb90e553bf343fbe
smmehrab <smmehrabul-2017614964@cs.du.ac.bd>
2022-09-03 08:02:20 +0600

    refactored enums/*

---
+++ src/enums/ComponentType.java
@@ -0,0 +1,26 @@
+package enums;
+
+public enum ComponentType {
+    Button("button"),
+    Editbox("editbox"),
+    Textbox("textbox"),
+    None("");
+
+    private String text;
+    ComponentType(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public static ComponentType fromString(String text) {
+        for (ComponentType componentType : ComponentType.values()) {
+            if (componentType.text.equalsIgnoreCase(text)) {
+                return componentType;
+            }
+        }
+        return null;
+    }
+}
